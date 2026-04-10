70a00ea19df1ebee5d36b29981e6e955a4c4b081
pipeespinosa <fracktiones@gmail.com>
2026-04-10 00:22:15 -0500

    fix: mitigación de SQLi con prepared statements, XSS con htmlspecialchars y validación de File Upload

--- vulnerabilities/sqli/source/low.php
+++ vulnerabilities/sqli/source/low.php
@@ -7,8 +7,10 @@ if( isset( $_REQUEST[ 'Submit' ] ) ) {
 	switch ($_DVWA['SQLI_DB']) {
 		case MYSQL:
 			// Check database
-			$query  = "SELECT first_name, last_name FROM users WHERE user_id = '$id';";
-			$result = mysqli_query($GLOBALS["___mysqli_ston"],  $query ) or die( '<pre>' . ((is_object($GLOBALS["___mysqli_ston"])) ? mysqli_error($GLOBALS["___mysqli_ston"]) : (($___mysqli_res = mysqli_connect_error()) ? $___mysqli_res : false)) . '</pre>' );
+			$stmt = $GLOBALS["___mysqli_ston"]->prepare("SELECT first_name, last_name FROM users WHERE user_id = ?");
+			$stmt->bind_param("s", $id);
+			$stmt->execute();
+			$result = $stmt->get_result();
 
 			// Get results
 			while( $row = mysqli_fetch_assoc( $result ) ) {
--- vulnerabilities/upload/source/low.php
+++ vulnerabilities/upload/source/low.php
@@ -1,19 +1,28 @@
 <?php
 
 if( isset( $_POST[ 'Upload' ] ) ) {
-	// Where are we going to be writing to?
-	$target_path  = DVWA_WEB_PAGE_TO_ROOT . "hackable/uploads/";
-	$target_path .= basename( $_FILES[ 'uploaded' ][ 'name' ] );
+    // Validación de tipo MIME y extensión
+    $allowed_types = array( 'image/jpeg', 'image/png', 'image/gif' );
+    $allowed_extensions = array( 'jpg', 'jpeg', 'png', 'gif' );
+    $file_extension = strtolower( pathinfo( $_FILES[ 'uploaded' ][ 'name' ], PATHINFO_EXTENSION ) );
 
-	// Can we move the file to the upload folder?
-	if( !move_uploaded_file( $_FILES[ 'uploaded' ][ 'tmp_name' ], $target_path ) ) {
-		// No
-		$html .= '<pre>Your image was not uploaded.</pre>';
-	}
-	else {
-		// Yes!
-		$html .= "<pre>{$target_path} succesfully uploaded!</pre>";
-	}
+    if( !in_array( $_FILES[ 'uploaded' ][ 'type' ], $allowed_types ) || !in_array( $file_extension, $allowed_extensions ) ) {
+        $html .= '<pre>Your image was not uploaded. Invalid file type.</pre>';
+    } else {
+        // Where are we going to be writing to?
+        $target_path  = DVWA_WEB_PAGE_TO_ROOT . "hackable/uploads/";
+        $target_path .= basename( $_FILES[ 'uploaded' ][ 'name' ] );
+
+        // Can we move the file to the upload folder?
+        if( !move_uploaded_file( $_FILES[ 'uploaded' ][ 'tmp_name' ], $target_path ) ) {
+            // No
+            $html .= '<pre>Your image was not uploaded.</pre>';
+        }
+        else {
+            // Yes!
+            $html .= "<pre>{$target_path} succesfully uploaded!</pre>";
+        }
+    }
 }
 
 ?>
